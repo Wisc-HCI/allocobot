0ffb051687720b99d0ca36220aba658a6e073ff0
Nathan White <ntwhite@wisc.edu>
2023-06-28 08:58:01 -0500

    Basic Q Learning

--- src/petri/matrix.rs
+++ src/petri/matrix.rs
@@ -1,12 +1,18 @@
 use nalgebra::base::DMatrix;
 use uuid::Uuid;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct MatrixNet {
     pub id: Uuid,
     pub name: String,
-    pub f: DMatrix<u128>,
-    pub v: DMatrix<u128>,
-    pub c: DMatrix<i128>,
-    pub marking: DMatrix<u128>
+    // F in most PetriNet representations
+    // Presence/absence of an arc between (places and transitions) and (transitions and places)
+    pub arcs: DMatrix<i64>,
+    // V in most PetriNet representations
+    // Weight (multiplicity) of the arcs
+    pub weights: DMatrix<i64>,
+    // C in most PetriNet representations
+    // Incidence is the change in each place for a given transition
+    pub incidence: DMatrix<i64>,
+    pub marking: DMatrix<i64>
 }
